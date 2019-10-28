5ebbd27e06f3a7fdd958190082d97c63fb8ae61a
Daniel Canetti <danielcanetti@daniels-mbp.home>
2019-10-28 21:43:27 +0000

    Init

---
+++ js/custom.js
@@ -0,0 +1,64 @@
+// Add Titles
+
+$("#add-cross-category-title").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td valign="top"><table align="right" border="0" cellpadding="0" cellspacing="0" width="600"><tr><td align="right" bgcolor="#eeeeee"><img alt="" height="75" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/cross-cat-title-grey.jpg" style="width: 341px;" width="341" /></td></tr>');
+});
+
+$("#add-tech-title").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td valign="top"><table align="right" border="0" cellpadding="0" cellspacing="0" width="600"><tr><td align="right" bgcolor="#eeeeee"><img alt="" height="75" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/tech-title-grey.jpg" style="width: 341px;" width="341" /></td></tr>');
+});
+
+$("#add-fs-title").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td valign="top"><table align="right" border="0" cellpadding="0" cellspacing="0" width="600"><tr><td align="right" bgcolor="#eeeeee"><img alt="" height="75" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/fs-title-grey.jpg" style="width: 341px;" width="341" /></td></tr>');
+});
+
+$("#add-trad-title").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td valign="top"><table align="right" border="0" cellpadding="0" cellspacing="0" width="600"><tr><td align="right" bgcolor="#eeeeee"><img alt="" height="75" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/trad-title-grey.jpg" style="width: 341px;" width="341" /></td></tr>');
+});
+
+$("#add-furn-title").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td valign="top"><table align="right" border="0" cellpadding="0" cellspacing="0" width="600"><tr><td align="right" bgcolor="#eeeeee"><img alt="" height="75" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/furniture-title-grey.jpg" style="width: 341px;" width="341" /></td></tr>');
+});
+
+$("#add-pubs-title").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td valign="top"><table align="right" border="0" cellpadding="0" cellspacing="0" width="600"><tr><td align="right" bgcolor="#eeeeee"><img alt="" height="75" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/pub-title-grey.jpg" style="width: 341px;" width="341" /></td></tr>');
+});
+
+// Add Text
+
+$("#add-text").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr><td valign="top"><table align="right" border="0" cellpadding="15" cellspacing="0" width="600"><tr><td align="left" style="padding: 15px; color:#333333; font-family: Arial, Helvetica, sans-serif; font-size: 14px;">Text</td></tr></table></td></tr><tr bgcolor="#ffffff"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td bgcolor="#eeeeee"><img alt="" height="17" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/grey-shadow.jpg" style="width: 600px;" width="600" /></td></tr>');
+});
+
+// Add Two Buttons
+
+$("#add-two-buttons").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td valign="top" width="590"><table border="0" cellpadding="0" cellspacing="0" width="590"><tbody><tr><td valign="top" width="290"><table align="center" border="0" cellpadding="0" cellspacing="0" width="270"><tbody><tr><td bgcolor="#ffffff"><img alt="MyPrint Link" height="170" src="http://mymarketing.voweurope.com/fck/4836/image/2017/01_jan_mn_09_01_17/GoSecure-MyPrint.jpg" style="width: 135px; height: 170px;" width="135"/></td><td bgcolor="#ffffff"><img alt="MyEmail Link" height="170" src="http://mymarketing.voweurope.com/fck/4836/image/2017/01_jan_mn_09_01_17/GoSecure-MyEmail.jpg" style="width: 135px; height: 170px;" width="135"/></td></tr></tbody></table><table align="center" border="0" cellpadding="0" cellspacing="0" width="270"><tbody><tr><td bgcolor="#eeeeee"><img alt="" height="17" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/grey-shadow.jpg" style="width: 270px; height: 17px;" width="270"/></td></tr></tbody></table></td><td valign="top" width="290"><table align="center" border="0" cellpadding="0" cellspacing="0" width="270"><tbody><tr><td bgcolor="#ffffff"><img alt="MyPrint Link" height="170" src="http://mymarketing.voweurope.com/fck/4836/image/2017/01_jan_mn_09_01_17/myCafe-MyPrint.jpg" style="width: 135px; height: 170px;" width="135"/></td><td bgcolor="#ffffff"><img alt="MyEmail Link" height="170" src="http://mymarketing.voweurope.com/fck/4836/image/2017/01_jan_mn_09_01_17/myCafe-MyEmail.jpg" style="width: 135px; height: 170px;" width="135"/></td></tr></tbody></table><table align="center" border="0" cellpadding="0" cellspacing="0" width="270"><tbody><tr><td bgcolor="#eeeeee"><img alt="" height="17" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/grey-shadow.jpg" style="width: 270px; height: 17px;" width="270"/></td></tr></tbody></table></td></tr></tbody></table></td></tr>');
+});
+
+// Add One Button
+
+$("#add-one-button").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td valign="top" width="590"><table align="center" border="0" cellpadding="0" cellspacing="0" width="590"><tbody><tr><td bgcolor="#ffffff"><img alt="MyPrint Link" height="170" src="http://mymarketing.voweurope.com/fck/4836/image/2017/01_jan_mn_09_01_17/bankers-box-MyPrint.jpg" style="width: 295px; height: 170px;" width="295"/></td><td bgcolor="#ffffff"><img alt="MyEmail Link" height="170" src="http://mymarketing.voweurope.com/fck/4836/image/2017/01_jan_mn_09_01_17/bankers-box-MyEmail.jpg" style="width: 295px; height: 170px;" width="295"/></td></tr></tbody></table></td></tr></tbody></table></td></tr><tr><td bgcolor="#eeeeee"><img alt="" height="17" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/grey-shadow.jpg" style="width: 600px;" width="600"/></td></tr>');
+});
+
+// Add Pubs Section
+
+$("#add-pubs-section").click(function(){
+    $("#eshot-content > td > table > tbody").append('<tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td valign="top"><table align="right" border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td align="right" bgcolor="#eeeeee"><img alt="" height="75" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/pub-title-grey.jpg" style="width: 341px;" width="341"/></td></tr></tbody></table></td></tr><tr><td valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr bgcolor="#ffffff"><td valign="top"><table align="right" border="0" cellpadding="10" cellspacing="0" width="100%"><tbody><tr><td align="left" style="color:#333333; font-family: Arial, Helvetica, sans-serif; font-size: 14px;"><ul><li>The Q1 Direct Mail deadline has passed, and delivery of all January catalogues will commence on 19th December. Any future orders can be placed via the VOW Publications Manager and will be delivered in batch two.</li><li>The 2017 VOWDigicat and VOWDigimailers are available to be ordered via the VOW Publications Manager. Order now to guarantee delivery of your link prior to Christmas.</li><li>For any further queries please contact the VOW Publications Team.​</li></ul></td></tr><tr><td valign="top"><table align="center" border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td align="center" bgcolor="#ffffff" style="font-family: Arial, Helvetica, sans-serif; font-size: 12px; color: #555555;" valign="top" width="300"><table align="center" border="0" cellpadding="10" cellspacing="0" width="95%"><tbody><tr><td bgcolor="#c1161f" style="text-align: center; color: rgb(255, 255, 255); font-family: Arial, Helvetica, sans-serif; font-size: 13px; font-weight: bold;"><a href="http://afb1.activepoint.com/ecat/digitalmagazine/VOW_Marketing_Guide_2017/enter.html" style="color: #ffffff; text-decoration: none;">Click here to view the<br/>Marketing Guide</a></td></tr></tbody></table></td><td align="center" bgcolor="#ffffff" style="font-family: Arial, Helvetica, sans-serif; font-size: 12px; color: #555555;" valign="top" width="300"><table align="center" border="0" cellpadding="10" cellspacing="0" width="95%"><tbody><tr><td bgcolor="#c1161f" style="text-align: center; color: rgb(255, 255, 255); font-family: Arial, Helvetica, sans-serif; font-size: 13px; font-weight: bold;"><a href="https://www.voweurope.com/home/ExternalPage.aspx?page=https://www.voweurope.com/apps/catorder/index.asp%3facc=&title=VOW%20Publications%20Manager" style="color: #ffffff; text-decoration: none;">Click here to order via the<br/>Publications Manager</a></td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></td></tr><tr bgcolor="#ffffff"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr><tr><td bgcolor="#eeeeee"><img alt="" height="17" src="http://mymarketing.voweurope.com/fck/4836/image/2016/12_dec_eshot_redesign/marketing_news/grey-shadow.jpg" style="width: 600px;" width="600"/></td></tr><tr bgcolor="#eeeeee"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td></tr>');
+});
+
+// Get code
+
+$("#get-code").click(function(){
+	
+	$('html,body').animate({
+        scrollTop: $("#get-code").offset().top},
+        'slow');
+	
+	var content = document.getElementById("eshot-wrapper").innerHTML;
+	var StrippedString = content.replace("<xmp>","");
+	var ReStrippedString = StrippedString.replace("</xmp>","");
+	
+	document.getElementById("copy-code-here").value = ReStrippedString;
+});
